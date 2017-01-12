f1b2bdcf4db801b37572a154f08952db55ada111
Nikita Kholin <nik.kholin@gmail.com>
2017-01-12 18:55:47 +0200

    Add initialize to StringCalculator

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,9 +1,17 @@
 # String calculator is responsible for making mathematical operations on numbers
 # in string
 class StringCalculator
-  def add(string)
+  def initialize(string)
+    @string = string
+  end
+
+  def add
     string.split(',')
           .map(&:to_f)
           .reduce(0, &:+)
   end
+
+  private
+
+  attr_accessor :string
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,16 +1,18 @@
 describe StringCalculator do
   describe '#add' do
-    let(:calculator) { StringCalculator.new }
     it 'should return 0 for empty string' do
-      expect(calculator.add('')).to eq 0
+      calculator = StringCalculator.new('0')
+      expect(calculator.add).to eq 0
     end
 
     it 'sould return that number if only one is given' do
-      expect(calculator.add('1.32')).to eq 1.32
+      calculator = StringCalculator.new('1.32')
+      expect(calculator.add).to eq 1.32
     end
 
     it 'sould return sum of numbers if 2 numbers are given' do
-      expect(calculator.add('1, 3.2')).to eq 4.2
+      calculator = StringCalculator.new('1,3.2')
+      expect(calculator.add).to eq 4.2
     end
   end
 end
