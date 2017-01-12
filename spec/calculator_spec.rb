92afb7e839bd2d4accbfa993d87a7a1f2ddb55e0
Nikita Kholin <nik.kholin@gmail.com>
2017-01-12 22:55:13 +0200

    Add unlimited amount of numbers support for Add method

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,18 +1,19 @@
 describe StringCalculator do
   describe '#add' do
     it 'should return 0 for empty string' do
-      calculator = StringCalculator.new('0')
-      expect(calculator.add).to eq 0
+      expect(StringCalculator.new('0').add).to eq 0
     end
 
     it 'sould return that number if only one is given' do
-      calculator = StringCalculator.new('1.32')
-      expect(calculator.add).to eq 1.32
+      expect(StringCalculator.new('1.32').add).to eq 1.32
     end
 
     it 'sould return sum of numbers if 2 numbers are given' do
-      calculator = StringCalculator.new('1,3.2')
-      expect(calculator.add).to eq 4.2
+      expect(StringCalculator.new('1,3.2').add).to eq 4.2
+    end
+
+    it 'sould return sum of numbers if any amount of numbers is given' do
+      expect(StringCalculator.new('1,3.2,5,-1,17').add).to eq 25.2
     end
   end
 end
