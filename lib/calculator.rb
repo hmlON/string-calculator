7e0a06afc861cd53ed6bedf3af15c6fded018773
Nikita Kholin <nik.kholin@gmail.com>
2017-01-12 18:37:04 +0200

    Fix rubocop issues

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,4 +1,6 @@
-class Calculator
+# String calculator is responsible for making mathematical operations on numbers
+# in string
+class StringCalculator
   def add(string)
     string.split(',')
           .map(&:to_f)
